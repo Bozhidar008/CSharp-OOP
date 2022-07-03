80f48b703638dfe35aed8112830f95e1b852430d
Bozhidar Marinov <bojidar008@gmail.com>
2022-07-03 18:02:59 +0300

    Add Solution MultipleInheritance

---
+++ Inheritance/MultipleInheritance/Puppy.cs
@@ -0,0 +1,15 @@
+﻿using _1.SingleInheritance;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleInheritance
+{
+    public class Puppy : Dog
+    {
+        public void Weep()
+        {
+            Console.WriteLine("weeping...");
+        }
+    }
+}
