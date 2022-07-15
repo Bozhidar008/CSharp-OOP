53c007fd70c381ea381a3d83e3648c9442d309fe
Bozhidar Marinov <bojidar008@gmail.com>
2022-07-15 10:57:40 +0300

    Add Solution Telephony

---
+++ InterfacesAndAbstractions/Telephony/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace Telephony
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string[] webSites = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var item in numbers)
+            {
+                
+                if (!item.Any(i=>char.IsLetter(i)) && item.Length == 7)
+                {
+                    Console.WriteLine($"Dialing... {item}");
+                }
+                else if (!item.Any(i=>char.IsLetter(i)) && item.Length == 10)
+                {
+                    Console.WriteLine($"Calling... {item}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid number!");
+                }
+            }
+
+            foreach (var item in webSites)
+            {
+                if (item.Any(i=>char.IsDigit(i)))
+                {
+                    Console.WriteLine("Invalid URL!");
+                }
+                else 
+                {
+                    Console.WriteLine($"Browsing: {item}!");
+                }
+            }
+        }
+    }
+}
---
+++ InterfacesAndAbstractions/Telephony/Smartphone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Telephony
+{
+    public class Smartphone : IPhone, IWeb
+    {
+        public void MakeACall(int number)
+        {
+            Console.WriteLine($"Calling...{number}");
+        }
+
+        public void SerchWeb(string site)
+        {
+            Console.WriteLine($"Browsing: {site}");
+        }
+    }
+}
