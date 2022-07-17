9d5b8d70566f84314ebf9d148247fc9f781d5454
Bozhidar Marinov <bojidar008@gmail.com>
2022-07-17 12:31:22 +0300

    Add Solution BorderControl

---
+++ InterfacesAndAbstractions/BorderControl/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BorderControl
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> ids = new List<string>();
+            string input;
+            while ( (input = Console.ReadLine()) != "End")
+            {
+                
+                string[] info = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (info.Length == 3)
+                {                   
+                    string id = info[2];
+                    ids.Add(id);                    
+                    continue;
+                }
+                else if (info.Length == 2)
+                {                    
+                    string id = info[1];
+                    ids.Add(id);
+                    continue;
+                }
+            }
+            string falceId = Console.ReadLine();
+
+            foreach (var item in ids)
+            {
+                if (item.EndsWith(falceId))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
---
+++ InterfacesAndAbstractions/BorderControl/Robot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+    internal class Robot : Identifier
+    {
+        private string model;
+        public Robot(string id, string model)
+        {
+            this.Id = id;
+            this.Model = model;
+        }
+        public string Id { get;}
+        public string Model { get; set; }
+    }
+}
