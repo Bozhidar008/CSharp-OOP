816ced65c5dc20f763b7833500895d0ab923a601
Bozhidar Marinov <bojidar008@gmail.com>
2022-07-22 11:44:30 +0300

    Add Solution SquareRoot

---
+++ 01.SquareRoot/01.SquareRoot/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _01.SquareRoot
+{
+    public class Program
+    {
+        static void Main()
+        {
+            
+
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+
+                if (n<0)
+                {
+                    throw new ArgumentException();
+                }
+
+
+                double num = Math.Sqrt(n);
+                Console.WriteLine(num);
+            }
+            catch (ArgumentException )
+            {
+
+                Console.WriteLine("Invalid number.");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
