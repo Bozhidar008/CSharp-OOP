a6c960c8aec379c15460ac8ed7d11c8ecfc03e85
Bozhidar Marinov <bojidar008@gmail.com>
2022-08-08 14:45:12 +0300

    Add Solution ValidationAttribute

---
+++ Reflection and Attributes/ValidationAttributes/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace ValidationAttributes.Models
+{
+    using CustomAttributes;
+    
+
+    public class Person
+    {
+        public Person(string fullName, int age)
+        {
+            this.Fullname = fullName;
+            this.Age = age;
+        }
+        [MyRequired]
+        public string Fullname { get; set; }
+        [MyRange(12,90)]
+        public int Age { get; set; }
+    }
+}
---
+++ Reflection and Attributes/ValidationAttributes/Models/Validator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace ValidationAttributes.Models
+{
+    using ValidationAttributes.CustomAttributes;
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            PropertyInfo[] properties = obj
+                .GetType()
+                .GetProperties();
+
+            foreach (PropertyInfo property in properties)
+            {
+                MyValidationAttribute customAttribute = 
+                    (MyValidationAttribute)property
+                    .GetCustomAttribute(typeof(MyValidationAttribute), false);
+                bool isValid = customAttribute.IsValid(property.GetValue(obj));
+                if (!isValid)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
